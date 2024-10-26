2902bcec9be8616a46ea2bd7a17c80ce62d2d8d5
Adarsh Learner <learner.adarsh@gmail.com>
2024-10-27 01:42:39 +0530

    update icons

--- packages/excalidraw/components/icons.tsx
+++ packages/excalidraw/components/icons.tsx
@@ -266,12 +266,9 @@ export const ExcalLogo = createIcon(
 
 // custom
 export const SelectionIcon = createIcon(
-  <g stroke="currentColor" strokeLinecap="round" strokeLinejoin="round">
-    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <path d="M6 6l4.153 11.793a0.365 .365 0 0 0 .331 .207a0.366 .366 0 0 0 .332 -.207l2.184 -4.793l4.787 -1.994a0.355 .355 0 0 0 .213 -.323a0.355 .355 0 0 0 -.213 -.323l-11.787 -4.36z" />
-    <path d="M13.5 13.5l4.5 4.5" />
-  </g>,
+  "/src/assets/Image/cursor.svg",
   { fill: "none", width: 22, height: 22, strokeWidth: 1.25 },
+  true
 );
 
 // tabler-icons: square
@@ -295,12 +292,9 @@ export const DiamondIcon = createIcon(
 
 // tabler-icons: circle
 export const EllipseIcon = createIcon(
-  <g strokeWidth="1.5">
-    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <circle cx="12" cy="12" r="9"></circle>
-  </g>,
-
+  "/src/assets/Image/circle-arrow.svg",
   tablerIconProps,
+  true
 );
 
 // tabler-icons: arrow-narrow-right
@@ -316,8 +310,9 @@ export const ArrowIcon = createIcon(
 
 // custom?
 export const LineIcon = createIcon(
-  <path d="M4.167 10h11.666" strokeWidth="1.5" />,
+  "/src/assets/Image/vectorslide.svg",
   modifiedTablerIconProps,
+  true
 );
 
 export const PenModeIcon = createIcon(
@@ -332,28 +327,16 @@ export const PenModeIcon = createIcon(
 
 // modified tabler-icons: pencil
 export const FreedrawIcon = createIcon(
-  <g strokeWidth="1.25">
-    <path
-      clipRule="evenodd"
-      d="m7.643 15.69 7.774-7.773a2.357 2.357 0 1 0-3.334-3.334L4.31 12.357a3.333 3.333 0 0 0-.977 2.357v1.953h1.953c.884 0 1.732-.352 2.357-.977Z"
-    />
-    <path d="m11.25 5.417 3.333 3.333" />
-  </g>,
-
+  "/src/assets/Image/edit-text.svg",
   modifiedTablerIconProps,
+  true
 );
 
 // tabler-icons: typography
 export const TextIcon = createIcon(
-  <g strokeWidth="1.5">
-    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <line x1="4" y1="20" x2="7" y2="20" />
-    <line x1="14" y1="20" x2="21" y2="20" />
-    <line x1="6.9" y1="15" x2="13.8" y2="15" />
-    <line x1="10.2" y1="6.3" x2="16" y2="20" />
-    <polyline points="5 20 11 4 13 4 20 20"></polyline>
-  </g>,
+  "/src/assets/Image/format.svg",
   tablerIconProps,
+  true
 );
 
 // modified tabler-icons: photo
@@ -1236,31 +1219,18 @@ export const ArrowheadArrowIcon = React.memo(
 export const ArrowheadCircleIcon = React.memo(
   ({ flip = false }: { flip?: boolean }) =>
     createIcon(
-      <g
-        stroke="currentColor"
-        fill="currentColor"
-        transform={flip ? "translate(40, 0) scale(-1, 1)" : ""}
-      >
-        <path d="M32 10L6 10" strokeWidth={2} />
-        <circle r="4" transform="matrix(-1 0 0 1 30 10)" />
-      </g>,
+      "/src/assets/Image/circle-arrow.svg",
       { width: 40, height: 20 },
+      true
     ),
 );
 
 export const ArrowheadCircleOutlineIcon = React.memo(
   ({ flip = false }: { flip?: boolean }) =>
     createIcon(
-      <g
-        stroke="currentColor"
-        fill="none"
-        transform={flip ? "translate(40, 0) scale(-1, 1)" : ""}
-        strokeWidth={2}
-      >
-        <path d="M26 10L6 10" />
-        <circle r="4" transform="matrix(-1 0 0 1 30 10)" />
-      </g>,
+      "/src/assets/Image/circle-arrow.svg",
       { width: 40, height: 20 },
+      true
     ),
 );
 
